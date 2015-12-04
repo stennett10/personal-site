1699ee374406214763a758687c4317471d2b0b5a
Joshua Stennett <Josh@Joshuas-MBP.home>
2015-12-04 20:54:03 +0000

    implement initial typed.js functionality

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,9 +4,25 @@
 	jQuery(document).ready(function(){
 		jQuery('a[data-gal]').each(function() {
 			jQuery(this).attr('rel', jQuery(this).data('gal'));
-		});  	
+		});
 		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
-	}); 
 
-		
+
+		console.log($(".typedStrings")[0]);
+
+
+
+		$(".typedStrings").typed({
+            strings: ["<something else>", "lorem ipsum", "geek"],
+						contentType: 'text',
+            typeSpeed: 80,
+						backSpeed: 50,
+						backDelay: 1000,
+						loop: true
+
+        });
+
+	});
+
+
 })(jQuery);
